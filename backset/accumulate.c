175ee3eb7ccad3b5f2171b19ea85a5b2b6db9db4
ttrsmmr <tetsuya.shimmura@gmail.com>
2013-10-10 00:46:08 +0900

    Create "Accumulate functions" for c language.

---
+++ backset/accumulate.c
@@ -0,0 +1,31 @@
+/** @file
+    Declaration on numeric values
+    
+    Copyright (c) 2013 ttysmmr
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt
+*/
+
+#include "numeric.h"
+
+#define DEFINE_ACCUMULATE_FUNCTION( suffix, type ) \
+    type accumulate_##suffix( type const* first, type const* last, type init ) \
+    { \
+        type sum = init; \
+        while( first != last ) { sum += *first++; } \
+        return sum; \
+    }
+
+DEFINE_ACCUMULATE_FUNCTION( char, char )
+DEFINE_ACCUMULATE_FUNCTION( uchar, unsigned char )
+DEFINE_ACCUMULATE_FUNCTION( short, short )
+DEFINE_ACCUMULATE_FUNCTION( ushort, unsigned short )
+DEFINE_ACCUMULATE_FUNCTION( int, int )
+DEFINE_ACCUMULATE_FUNCTION( uint, unsigned int )
+DEFINE_ACCUMULATE_FUNCTION( long, long )
+DEFINE_ACCUMULATE_FUNCTION( ulong, unsigned long )
+DEFINE_ACCUMULATE_FUNCTION( float, float )
+DEFINE_ACCUMULATE_FUNCTION( double, double )
+
+#undef DEFINE_ACCUMULATE_FUNCTION
---
+++ backset/accumulate.h
@@ -0,0 +1,44 @@
+#if !defined( NUMERIC_H_20131009191253 )
+#define NUMERIC_H_20131009191253
+
+/** @file
+    Declaration on accumulate functions.
+    
+    Copyright (c) 2013 ttysmmr
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt
+*/
+
+/// Accumulate values in range.
+/// @return The result of accumulating init and all the elements in the range [first,last).
+char accumulate_char( char const* first, char const* last, char init );
+
+/// @overload accumulate_char
+unsigned char accumulate_uchar( unsigned char const* first, unsigned char const* last, unsigned char init );
+
+/// @overload accumulate_char
+short accumulate_short( short const* first, short const* last, short init );
+
+/// @overload accumulate_char
+unsigned short accumulate_ushort( unsigned short const* first, unsigned short const* last, unsigned short init );
+
+/// @overload accumulate_char
+int accumulate_int( int const* first, int const* last, int init );
+
+/// @overload accumulate_char
+unsigned int accumulate_uint( unsigned int const* first, unsigned int const* last, unsigned int init );
+
+/// @overload accumulate_char
+long accumulate_long( long const* first, long const* last, long init );
+
+/// @overload accumulate_char
+unsigned long accumulate_ulong( unsigned long const* first, unsigned long const* last, unsigned long init );
+
+/// @overload accumulate_char
+float accumulate_float( float const* first, float const* last, float init );
+
+/// @overload accumulate_char
+double accumulate_double( double const* first, double const* last, double init );
+
+#endif // #if !defined( NUMERIC_H_20131009191253 )
